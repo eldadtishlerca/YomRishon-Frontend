e255d57d1b3a125fe1032bcce295cc4b58af2db2
Yarden Shaul <yarden727@gmail.com>
2022-05-25 16:18:26 +0300

    add nevigate

--- src/cmps/side-bar.jsx
+++ src/cmps/side-bar.jsx
@@ -1,15 +1,20 @@
+import { Nav, Link, Routes, Route, useNavigate } from 'react-router-dom'
 import { UserNotflication } from './user-notflication'
 import { UserProfile } from '../pages/user-profile-page'
 import { UserWork } from './user-work'
 import { Search } from './search'
 
 export function SideBar() {
+  let nevigate = useNavigate()
   return (
     <div>
-      <UserNotflication />
-      <UserWork />
-      <Search />
-      <UserProfile />
+        <button onClick={() => {nevigate("/user")}}>user profile</button>
+        <button onClick={() => {nevigate("/")}}>Home Page</button>
+
     </div>
   )
 }
+
+// homepage
+// nav - [usernortifications, searh, user work]
+// user profile
