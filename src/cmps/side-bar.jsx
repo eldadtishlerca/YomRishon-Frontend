b942b93bf36e1aaf703d61f3d46044c439f5bdf4
eldadtishlerca <eldad55555@gmail.com>
2022-05-25 16:20:36 +0300

    fix flow

--- src/cmps/side-bar.jsx
+++ src/cmps/side-bar.jsx
@@ -1,6 +1,5 @@
-import { Nav, Link, Routes, Route, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { UserNotflication } from './user-notflication'
-import { UserProfile } from '../pages/user-profile-page'
 import { UserWork } from './user-work'
 import { Search } from './search'
 
@@ -8,13 +7,23 @@ export function SideBar() {
   let nevigate = useNavigate()
   return (
     <div>
-        <button onClick={() => {nevigate("/user")}}>user profile</button>
-        <button onClick={() => {nevigate("/")}}>Home Page</button>
-
+      <button
+        onClick={() => {
+          nevigate('/')
+        }}
+      >
+        Home Page
+      </button>
+      <UserNotflication />
+      <UserWork />
+      <Search />
+      <button
+        onClick={() => {
+          nevigate('/user')
+        }}
+      >
+        user profile
+      </button>
     </div>
   )
 }
-
-// homepage
-// nav - [usernortifications, searh, user work]
-// user profile
