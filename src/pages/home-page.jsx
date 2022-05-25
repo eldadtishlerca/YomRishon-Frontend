b5f72c52bd06e1f55966cc3c199d02b073dc510b
Elon Barzani <elon5000@gmail.com>
2022-05-25 17:19:50 +0300

    homepage routing

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom"
+
 export function HomePage() {
-  return <div>Im HOME PAGE</div>
+
+  const navigate = useNavigate()
+
+  return <div>
+    <header>
+      <h1>Logo</h1>
+      <button onClick={()=> {navigate('/loginsignup')}}>Login</button>
+    </header>
+    <main>
+      <div>
+        <h1>Feel the workflow</h1>
+        <h3>Manage your work with powerful tools</h3>
+        <button>Get Started</button>
+      </div>
+    </main>
+  </div>
 }
