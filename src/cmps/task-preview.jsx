af6ef241edf3d6cd6f78cdb19c84e2726676bd17
Yarden Shaul <yarden727@gmail.com>
2022-05-25 17:02:15 +0300

    add route to task-details

--- src/cmps/task-preview.jsx
+++ src/cmps/task-preview.jsx
@@ -1,13 +1,24 @@
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
 import { TaskModal } from './task-modal'
 import { TaskTitle } from './task-title'
 import { TaskActivites } from './task-activites'
 
 export function TaskPreview() {
+  let nevigate = useNavigate()
+
+  const onOpenTask = () => {
+    <Routes>
+        <Route path="/:groupId/:taskId" />
+    </Routes>
+    nevigate("/:groupId/:taskId")
+  }
+
   return (
-    <div className="flex">
+    <div className="flex" onClick={() => onOpenTask}>
       <TaskModal />
       <TaskTitle />
       <TaskActivites />
+
     </div>
   )
 }
