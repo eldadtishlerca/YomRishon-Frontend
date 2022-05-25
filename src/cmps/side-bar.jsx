3a26c70974ce327aa8e337cd0e6e3009dce71d84
Yarden Shaul <yarden727@gmail.com>
2022-05-25 14:56:24 +0300

    side bar

--- src/cmps/side-bar.jsx
+++ src/cmps/side-bar.jsx
@@ -1,3 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {UserNotflication} from './user-notflication'
+import {UserProfile} from '../pages/user-profile'
+import {UserWork} from './user-work'
+import {Search} from './search'
+
 export function SideBar() {
-    return <div>IM SideBar</div>
+    return <div>IM SideBar
+        <UserNotflication />
+        <UserWork />
+        <Search /> 
+        
+    </div>
 }
