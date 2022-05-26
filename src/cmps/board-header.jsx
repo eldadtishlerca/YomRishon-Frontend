ff9e8b043891f1baa1792e3fb006bcd50e4b51e3
Yarden Shaul <yarden727@gmail.com>
2022-05-26 13:12:40 +0300

    add scss to header

--- src/cmps/board-header.jsx
+++ src/cmps/board-header.jsx
@@ -1,26 +1,35 @@
 import { ToolBar } from '../cmps/toolbar'
 
 import { MdOutlineTableChart } from 'react-icons/md'
+import { AiOutlineStar } from 'react-icons/ai'
 
 export const BoardHeader = ({ members, title, activities }) => {
   return (
-    <div className='board-header-main-container'>
+    <div className='board-header-main-container flex'>
       <div className="board-header-top flex">
-        <h1>{title}</h1>
         <div className="flex">
+          <h1>{title}</h1>
+          <AiOutlineStar />
+        </div>
+        <div className="board-header-top-main flex">
+          <div className="flex">
+          <span>Last seen</span>
+          <div className="flex">
           {members.map((member) => (
             <img key={member._id} src={member.imgUrl} alt={member.fullname} />
           ))}
         </div>
+          </div>
         <div>
           <button>Activity</button>
         </div>
         <div>
           <button>Add to Board</button>
         </div>
+        </div>
       </div>
       <div className="board-header-bottom flex">
-        <div>
+        <div className="board-header-bottom-upper flex">
           <MdOutlineTableChart />
           <span>Main Table</span>
         </div>
--- src/cmps/toolbar.jsx
+++ src/cmps/toolbar.jsx
@@ -1,24 +1,28 @@
 import { AiOutlineSearch } from 'react-icons/ai'
 import { CgProfile, CgColorBucket } from 'react-icons/cg'
 import { BiFilterAlt, BiSortAlt2 } from 'react-icons/bi'
+import { IoIosArrowDropdown } from 'react-icons/io'
 
 export const ToolBar = () => {
   return (
-    <div className="toolbar flex">
-      <div>New Project</div>
-      <div>
+    <div className="toolbar-main-contianer flex">
+      <div className="toolbar-button-container flex">
+        <span className='new-task-spn'>New Task</span>
+        <IoIosArrowDropdown />
+      </div>
+      <div className="toolbar-button-container flex">
         <AiOutlineSearch />
         <span>Search</span>
       </div>
-      <div>
+      <div className="toolbar-button-container flex">
         <CgProfile />
         <span>Person</span>
       </div>
-      <div>
+      <div className="toolbar-button-container flex">
         <BiFilterAlt />
         <span>Filter</span>
       </div>
-      <div>
+      <div className="toolbar-button-container flex">
         <BiSortAlt2 />
         <span>Sort</span>
       </div>
