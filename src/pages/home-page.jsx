0fe25e3b0ce6ab6c85252d590f90f06b4404332b
Elon Barzani <elon5000@gmail.com>
2022-05-26 11:38:46 +0300

    homepage done

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,22 +1,51 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from 'react-router-dom'
 
 export const HomePage = () => {
-
   const navigate = useNavigate()
 
-  return <div className="homepage">
-    <header className="homepage-header">
-      <h1 className="logo">
-        <span className="yom">Yom</span><span className="ris">Ris</span><span className="hon">hon</span>
+  return (
+    <div className="homepage">
+      <header className="homepage-header">
+        <h1 className="logo">
+          <span className="yom">Yom</span>
+          <span className="ris">Ris</span>
+          <span className="hon">hon</span>
         </h1>
-      <button className="login-btn" onClick={()=> {navigate('/loginsignup')}}>Login</button>
-    </header>
-    <main>
-      <div className="hero">
-        <h1>Feel the workflow</h1>
-        <h3>Manage your work with powerful tools</h3>
-      <button className="hero-btn" onClick={()=> {navigate('/board')}}>Get Started</button>
-      </div>
-    </main>
-  </div>
+        <button
+          className="login-btn"
+          onClick={() => {
+            navigate('/loginsignup')
+          }}
+        >
+          Login
+        </button>
+      </header>
+      <main>
+        <section className='shooting-stars'>
+          <span className='star'></span>
+          <span className='star'></span>
+          <span className='star'></span>
+          <span className='star'></span>
+          <span className='star'></span>
+          <span className='star'></span>
+          <span className='star'></span>
+          <span className='star'></span>
+        </section>
+        <div className="hero">
+          <h1>Feel the workflow</h1>
+          <h3>Manage your work with powerful tools</h3>
+          <div className="hero-btn-wrapper">
+            <button
+              className="hero-btn"
+              onClick={() => {
+                navigate('/board')
+              }}
+            >
+              Get Started
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
