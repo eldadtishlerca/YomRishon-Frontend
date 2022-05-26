72037edeac6ea1d91abc86db2e5ce099f987b44a
eldadtishlerca <eldad55555@gmail.com>
2022-05-26 10:29:28 +0300

    add toolbar items

--- src/cmps/toolbar.jsx
+++ src/cmps/toolbar.jsx
@@ -1,3 +1,30 @@
+import { AiOutlineSearch } from 'react-icons/ai'
+import { CgProfile, CgColorBucket } from 'react-icons/cg'
+import { BiFilterAlt, BiSortAlt2 } from 'react-icons/bi'
+
 export const ToolBar = () => {
-    return <div>IM TOOLBAR</div>
-  }
+  return (
+    <div className="toolbar flex">
+      <div>New Project</div>
+      <div>
+        <AiOutlineSearch />
+        <span>Search</span>
+      </div>
+      <div>
+        <CgProfile />
+        <span>Person</span>
+      </div>
+      <div>
+        <BiFilterAlt />
+        <span>Filter</span>
+      </div>
+      <div>
+        <BiSortAlt2 />
+        <span>Sort</span>
+      </div>
+      {/* <div>
+        <CgColorBucket />
+      </div> */}
+    </div>
+  )
+}
