0c623138c7f2a3d698487c50cd44a66a902cc039
Elon Barzani <elon5000@gmail.com>
2022-05-26 09:39:19 +0300

    more sass on homepage

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -12,9 +12,11 @@ export function HomePage() {
       <button className="login-btn" onClick={()=> {navigate('/loginsignup')}}>Login</button>
     </header>
     <main>
+      <div className="hero">
         <h1>Feel the workflow</h1>
         <h3>Manage your work with powerful tools</h3>
       <button className="hero-btn" onClick={()=> {navigate('/board')}}>Get Started</button>
+      </div>
     </main>
   </div>
 }
