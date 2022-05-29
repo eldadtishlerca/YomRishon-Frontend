09144bb3aa6f5bc75133c057b2c020538aa8b169
eldadtishlerca <eldad55555@gmail.com>
2022-05-29 19:34:04 +0300

    add async storage

--- src/store/actions/board.actions.js
+++ src/store/actions/board.actions.js
@@ -1,12 +1,17 @@
 import { boardService } from '../../services/board.service'
 
-// var subscriber
+export function getActionBoard(board) {
+  return {
+    type: 'SET_BOARD',
+    board,
+  }
+}
 
 export function loadBoard(boardId) {
-  return async (dispatch, getState) => {
+  return async (dispatch) => {
     try {
       const board = await boardService.query(boardId)
-      dispatch({ type: 'SET_BOARD', board })
+      dispatch(getActionBoard(board))
     } catch (err) {
       console.log('Cannot load board', err)
     }
@@ -15,15 +20,16 @@ export function loadBoard(boardId) {
 
 export function updateBoard(board) {
   return (dispatch) => {
-      boardService.save(board)
-          .then(savedBoard => {
-              console.log('Updated Board:', savedBoard);
-              // dispatch(getActionUpdateBoard(savedBoard))
-              // showSuccessMsg('Board updated')
-          })
-          .catch(err => {
-              // showErrorMsg('Cannot update board')
-              console.log('Cannot save board', err)
-          })
+    boardService
+      .save(board)
+      .then((savedBoard) => {
+        console.log('Updated Board:', savedBoard)
+        // dispatch(getActionUpdateBoard(savedBoard))
+        // showSuccessMsg('Board updated')
+      })
+      .catch((err) => {
+        // showErrorMsg('Cannot update board')
+        console.log('Cannot save board', err)
+      })
   }
 }
