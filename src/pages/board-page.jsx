163def66c84ebc5c38fea0e2567234f3a418f6b2
eldadtishlerca <eldad55555@gmail.com>
2022-05-30 11:47:37 +0300

    add folders

--- src/pages/board-page.jsx
+++ src/pages/board-page.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { BoardHeader } from '../cmps/board-header'
-import { BoardContent } from '../cmps/board-content'
-import { MainSidebar } from '../cmps/main-sidebar'
+import { BoardHeader } from '../cmps/board/board-header'
+import { BoardContent } from '../cmps/board/board-content'
+import { MainSidebar } from '../cmps/sidebars/main-sidebar'
 import { loadBoard } from '../store/actions/board.actions'
-import { WorkspaceSidebarClosed } from '../cmps/workspace-sidebar-closed'
-import { BoardNotifications } from '../cmps/board-nortifications'
-import { WorkspaceSidebar } from '../cmps/workspace-sidebar'
+import { WorkspaceSidebarClosed } from '../cmps/sidebars/workspace-sidebar-closed'
+import { BoardNotifications } from '../cmps/sidebars/board-nortifications'
+import { WorkspaceSidebar } from '../cmps/sidebars/workspace-sidebar'
 
 export const BoardPage = () => {
   const { board } = useSelector((storeState) => storeState.boardModule)
