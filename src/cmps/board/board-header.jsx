6bef9aa93ec624f79fc20a7459420d4e258631f5
Elon Barzani <elon5000@gmail.com>
2022-05-31 20:11:24 +0300

    bug fixxed

--- src/cmps/board/board-header.jsx
+++ src/cmps/board/board-header.jsx
@@ -8,7 +8,7 @@ import { BsPlus } from 'react-icons/bs'
 
 export const BoardHeader = () => {
   const { currBoard } = useSelector((storeState) => storeState.boardModule)
-  const { members, title } = currBoard
+  const { members } = currBoard
   const [titleValue, setTitleValue] = useState(currBoard.title)
   const [descriptionValue, setDescriptionValue] = useState(currBoard.description || '')
   const dispatch = useDispatch()
