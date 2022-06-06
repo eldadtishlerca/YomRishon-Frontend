d77bc05f5d266fa7995f6ccede46a992d2be3544
eldadtishlerca <eldad55555@gmail.com>
2022-06-06 16:15:29 +0300

    fix more

--- src/cmps/task/task-details.jsx
+++ src/cmps/task/task-details.jsx
@@ -161,7 +161,7 @@ export const TaskDetails = ({ setShowModal, task, groupId }) => {
             </div>
           </div>
                 )
-              })
+              }
         </div>
       </div>
     </div>
