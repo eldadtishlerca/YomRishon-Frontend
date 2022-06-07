6d88cc9ef46e57bc6815f9872048cd779230d436
Yarden Shaul <yarden727@gmail.com>
2022-06-07 11:52:50 +0300

    .

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -11,7 +11,7 @@ const SOCKET_EMIT_LOGIN = 'set-user-socket';
 const SOCKET_EMIT_LOGOUT = 'unset-user-socket';
 
 
-const baseUrl = (process.env.NODE_ENV === 'production') ? '' : '//localhost:3030'
+const baseUrl = (process.env.NODE_ENV === 'production') ? '' : '//localhost:3030/api/'
 export const socketService = createSocketService()
 // export const socketService = createDummySocketService()
 
